7c9a808d1400d446b00533d5e7b22523507f8e15
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2019-11-09 12:04:54 -0800

    Add files via upload

--- Sprint_Challenge/acme_test.py
+++ Sprint_Challenge/acme_test.py
@@ -13,9 +13,19 @@ class AcmeProductTests(unittest.TestCase):
         self.assertEqual(prod.price, 10)
 
     def test_default_product_weight(self):
-        """Test default product weiight being 20."""
+        """Test default product weight being 20."""
         prod = Product('Test Product')
         self.assertEqual(prod.weight, 20)
+
+    def test_default_stealability(self):
+        """Test default stealability."""
+        prod = Product('Test Product', price=10, weight=40)
+        self.assertEqual((prod.price / prod.weight), 0.25)
+
+    def test_default_explode(self):
+        """Test default explode."""
+        prod = Product('Test Product', weight=40, flammability=2.0)
+        self.assertEqual((prod.flammability * prod.weight), 80)
 
 
 if __name__ == '__main__':
